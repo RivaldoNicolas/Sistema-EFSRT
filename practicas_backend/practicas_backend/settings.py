b499d0861df5dd50d83ba435217b47356c539834
Rivaldo <rivaldonicolasfabian@gmail.com>
2024-12-10 12:11:41 -0500

    Update settings.py

--- practicas_backend/practicas_backend/settings.py
+++ practicas_backend/practicas_backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         "NAME": "efsrt_db",
         "USER": "root",
         "PASSWORD": "",
-        "HOST": "127.0.0.1",
+        "HOST": "mysql",
         "PORT": "3306",
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
